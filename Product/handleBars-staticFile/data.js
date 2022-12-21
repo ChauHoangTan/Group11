b546c1046c6a46d5547e0cb7991548905c1f156c
ChauHoangTan <20127621@student.hcmus.edu.vn>
2022-12-21 14:36:24 +0700

    update folder product

---
+++ Product/handleBars-staticFile/data.js
@@ -0,0 +1,25 @@
+
+const products = [
+    {
+        imgPath1 : "handleBars-staticFile/redmiNote8Pro1.jpg",
+        imgPath2 : "handleBars-staticFile/redmiNote8Pro2.jpg",
+        imgPath3 : "handleBars-staticFile/redmiNote8Pro3.jpg"
+    },
+    {
+        imgPath1 : "handleBars-staticFile/redmiNote8Pro1.jpg",
+        imgPath2 : "handleBars-staticFile/redmiNote8Pro2.jpg",
+        imgPath3 : "handleBars-staticFile/redmiNote8Pro3.jpg"
+    },
+    {
+        imgPath1 : "handleBars-staticFile/redmiNote8Pro1.jpg",
+        imgPath2 : "handleBars-staticFile/redmiNote8Pro2.jpg",
+        imgPath3 : "handleBars-staticFile/redmiNote8Pro3.jpg"
+    },
+    {
+        imgPath1 : "handleBars-staticFile/redmiNote8Pro1.jpg",
+        imgPath2 : "handleBars-staticFile/redmiNote8Pro2.jpg",
+        imgPath3 : "handleBars-staticFile/redmiNote8Pro3.jpg"
+    }
+]
+
+
